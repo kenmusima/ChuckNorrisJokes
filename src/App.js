a2e1bcf8c98b0668b74bc474b4a8427fbc71043e
kenmusima <ksmusima@gmail.com>
2020-12-15 22:45:05 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      data: [],
+      subdata: [],
+      isLoaded: false,
+    }
+  }
+  componentDidMount() {
+    fetch('https://api.chucknorris.io/jokes/categories')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          data : json,
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        })
+      });
+
+  }
+  handleClick(param) {
+    fetch('https://api.chucknorris.io/jokes/random?categories?=' + param)
+      .then(rest => rest.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          subdata : json,
+        })
+      });
+    
+  }
+  render() {
+    var { isLoaded, data , subdata} = this.state;
+    //console.log(data)
+    if(!isLoaded){
+      return <div>Loading...</div>;
+    }
+    else {
+      return (
+        <div className="App">
+          <div className="container">
+            { data.map(dat => (
+                <button onClick={() => this.handleClick(dat)}>
+                  {dat} {console.log(dat)}
+                </button>
+              ))}
+            <p>
+            {subdata.value}
+            </p>
+          </div>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
